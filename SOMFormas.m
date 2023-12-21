1e66040269b20b9dd361a7886a4b034c11eff409
MAngelGC <magc6102@uma.es>
2023-12-21 04:46:32 +0100

    gitignore

--- SOMFormas.m
+++ SOMFormas.m
@@ -9,17 +9,15 @@ IterMax=1000;
 FC=[8 6];
  
 [NumDatos,~]=size(data);
-W=rand(NumA,FC(1),FC(2));
 W=GenerarSOMcuadrada(FC);
 
 DibujarW(W,FC);
 DibujarD(data)
 axis([-1.5 1.5 -1.5 1.5])
-pause
 
 Indices=GenerarIndices(FC);
 for i=1:IterMax
-    fprintf('i: %d\n',i);
+    % fprintf('i: %d\n',i);
     ind=randperm(NumDatos);
     for j=1:NumDatos
         eta=eta0*(1-i/IterMax);
@@ -28,18 +26,18 @@ for i=1:IterMax
         [Gx,Gy]=CalculoGanadora(W,Patron);
         IndGan=[Gx,Gy]';
         
-        Vecindad=FuncionVecindadConDistancia(IndGan,W,Indices);
+        Vecindad=FuncionVecindadCompetitiva(IndGan,W,Indices);
         W=IncrementarPesos(W,Patron,Vecindad,eta);
 
     end
     
 
-    if mod(i,10)==0
-        DibujarW(W,FC);
-        DibujarD(data)
-        axis([-1.5 1.5 -1.5 1.5])
-        drawnow
-    end
+    %if mod(i,10)==0
+     %   DibujarW(W,FC);
+     %   DibujarD(data)
+     %  axis([-1.5 1.5 -1.5 1.5])
+     %   drawnow
+    %end
 
 end
 
